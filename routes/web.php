6b7ca2d178f525123a687bfcdcd8a4c4e5f2d76a
Yuko <n.nikita.6607@gmail.com>
2021-01-29 17:56:48 +0900

    Group化 commit

--- routes/web.php
+++ routes/web.php
@@ -24,34 +24,34 @@ Route::get('/memo', 'MemoController@index');
 Route::get('/targetschool', 'TargetschoolController@index');
 Route::get('admin/', 'Admin\HomeController@home');
 
-Route::group(['prefix' => 'admin'], function () {
-  Route::get('homework/create', 'Admin\HomeworkController@add')->middleware('auth');
-  Route::post('homework/create', 'Admin\HomeworkController@create')->middleware('auth');
-  Route::get('homework/edit', 'Admin\HomeworkController@edit')->middleware('auth');
-  Route::post('homework/edit', 'Admin\HomeworkController@update')->middleware('auth');
-  Route::get('homework/', 'Admin\HomeworkController@index')->middleware('auth');
-  Route::post('homework/delete', 'Admin\HomeworkController@delete')->middleware('auth');
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function () {
+  Route::get('homework/create', 'Admin\HomeworkController@add');
+  Route::post('homework/create', 'Admin\HomeworkController@create');
+  Route::get('homework/edit', 'Admin\HomeworkController@edit');
+  Route::post('homework/edit', 'Admin\HomeworkController@update');
+  Route::get('homework/', 'Admin\HomeworkController@index');
+  Route::post('homework/delete', 'Admin\HomeworkController@delete');
 
-  Route::get('targetschool/create', 'Admin\TargetschoolController@add')->middleware('auth');
-  Route::get('targetschool/create', 'Admin\TargetschoolController@create')->middleware('auth');
-  Route::get('targetschool/edit', 'Admin\TargetschoolController@edit')->middleware('auth');
-  Route::post('targetschool/edit', 'Admin\TargetschoolController@update')->middleware('auth');
-  Route::get('targetschool/', 'Admin\TargetschoolController@index')->middleware('auth');
-  Route::post('targetschool/delete', 'Admin\TargetschoolController@delete')->middleware('auth');
+  Route::get('targetschool/create', 'Admin\TargetschoolController@add');
+  Route::get('targetschool/create', 'Admin\TargetschoolController@create');
+  Route::get('targetschool/edit', 'Admin\TargetschoolController@edit');
+  Route::post('targetschool/edit', 'Admin\TargetschoolController@update');
+  Route::get('targetschool/', 'Admin\TargetschoolController@index');
+  Route::post('targetschool/delete', 'Admin\TargetschoolController@delete');
 
-  Route::get('standardscore/create', 'Admin\StandardscoreController@add')->middleware('auth');
-  Route::post('standardscore/create', 'Admin\StandardscoreController@create')->middleware('auth');
-  Route::get('standardscore/edit', 'Admin\StandardscoreController@edit')->middleware('auth');
-  Route::post('standardscore/edit', 'Admin\StandardscoreController@update')->middleware('auth');
-  Route::get('standardscore/', 'Admin\StandardscoreController@index')->middleware('auth');
-  Route::post('standardscore/delete', 'Admin\StandardscoreController@delete')->middleware('auth');
+  Route::get('standardscore/create', 'Admin\StandardscoreController@add');
+  Route::post('standardscore/create', 'Admin\StandardscoreController@create');
+  Route::get('standardscore/edit', 'Admin\StandardscoreController@edit');
+  Route::post('standardscore/edit', 'Admin\StandardscoreController@update');
+  Route::get('standardscore/', 'Admin\StandardscoreController@index');
+  Route::post('standardscore/delete', 'Admin\StandardscoreController@delete');
 
-  Route::get('memo/create', 'Admin\MemoController@add')->middleware('auth');
-  Route::post('memo/create', 'Admin\MemoController@create')->middleware('auth');
-  Route::get('memo/edit', 'Admin\MemoController@edit')->middleware('auth');
-  Route::post('memo/edit', 'Admin\MemoController@update')->middleware('auth');
-  Route::get('memo/', 'Admin\MemoController@index')->middleware('auth');
-  Route::post('memo/delete', 'Admin\MemoController@delete')->middleware('auth');
+  Route::get('memo/create', 'Admin\MemoController@add');
+  Route::post('memo/create', 'Admin\MemoController@create');
+  Route::get('memo/edit', 'Admin\MemoController@edit');
+  Route::post('memo/edit', 'Admin\MemoController@update');
+  Route::get('memo/', 'Admin\MemoController@index');
+  Route::post('memo/delete', 'Admin\MemoController@delete');
 });
 
 Auth::routes();
